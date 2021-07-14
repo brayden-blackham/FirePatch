972e7b4d4e478c37cd8dc5702f769881a1d31536
Brayden Blackham <blackhambrayden@gmail.com>
2021-07-14 18:01:32 +0800

    Working on VSCode

--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -10,11 +10,17 @@ import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650
-  }
+    minWidth: 650,
+  },
 });
 
-function createData(name, calories, fat, carbs, protein) {
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
   return { name, calories, fat, carbs, protein };
 }
 
@@ -23,7 +29,7 @@ const rows = [
   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
   createData("Eclair", 262, 16.0, 24, 6.0),
   createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9)
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
 export default function BasicTable() {
